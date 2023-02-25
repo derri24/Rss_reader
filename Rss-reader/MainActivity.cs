3cb9707d4918121b867652218400bf677ff8defa
tranquility09 <derri5437@gmail.com>
2023-02-25 22:20:50 +0300

    load

---
+++ Rss-reader/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Webkit;
+using Android.Widget;
+using Rss_reader.rss_client.Models;
+
+namespace Rss_reader
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var searchBtn = (Button) FindViewById(Resource.Id.searchBtn);
+            if (searchBtn != null)
+                searchBtn.Click += delegate { SearchBtn_Click(); };
+        }
+
+        private async Task SearchBtn_Click()
+        {
+            var source = ((EditText) FindViewById(Resource.Id.@ref))?.Text;
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync("https://www.onliner.by/feed").Result;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                HttpContent responseContent = response.Content;
+                var xml = await responseContent.ReadAsStringAsync();
+
+                XmlSerializer serializer = new XmlSerializer(typeof(RssModel));
+                TextReader reader = new StringReader(xml);
+                var rssModel = (RssModel) serializer.Deserialize(reader);
+
+
+                WebView webView = (WebView) FindViewById(Resource.Id.webview);
+// static html string data
+                string customHtml = "<html><body><h1>Hello, AbhiAndroid</h1>" +
+                                    "<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3>" +
+                                    "<p>This is a sample paragraph of static HTML In Web view</p>" +
+                                    "</body></html>";
+// load static html data on a web view
+                if (webView != null)
+                    webView.LoadData(customHtml, "text/html", "UTF-8");
+            }
+        }
+    }
+}
---
+++ Rss-reader/RssModel.cs
@@ -0,0 +1,104 @@
+﻿namespace Rss_reader
+{
+   using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace rss_client.Models
+{
+    [XmlRoot(ElementName = "image")]
+    public class Image
+    {
+        [XmlElement(ElementName = "url")] public string Url { get; set; }
+        [XmlElement(ElementName = "width")] public string Width { get; set; }
+        [XmlElement(ElementName = "height")] public string Height { get; set; }
+        [XmlElement(ElementName = "title")] public string Title { get; set; }
+        [XmlElement(ElementName = "link")] public string Link { get; set; }
+    }
+
+    [XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "href")] public string Href { get; set; }
+        [XmlAttribute(AttributeName = "rel")] public string Rel { get; set; }
+        [XmlAttribute(AttributeName = "type")] public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "guid")]
+    public class Guid
+    {
+        [XmlAttribute(AttributeName = "isPermaLink")]
+        public string IsPermaLink { get; set; }
+
+        [XmlText] public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "thumbnail", Namespace = "http://search.yahoo.com/mrss/")]
+    public class Thumbnail
+    {
+        [XmlAttribute(AttributeName = "url")] public string Url { get; set; }
+    }
+
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")] public string Title { get; set; }
+        [XmlElement(ElementName = "link")] public string Link2 { get; set; }
+        [XmlElement(ElementName = "pubDate")] public string PubDate { get; set; }
+
+        [XmlElement(ElementName = "creator", Namespace = "http://purl.org/dc/elements/1.1/")]
+        public string Creator { get; set; }
+
+        [XmlElement(ElementName = "category")] public string Category { get; set; }
+        [XmlElement(ElementName = "guid")] public Guid Guid { get; set; }
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+
+        [XmlElement(ElementName = "thumbnail", Namespace = "http://search.yahoo.com/mrss/")]
+        public Thumbnail Thumbnail { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "title")] public string Title { get; set; }
+        [XmlElement(ElementName = "link")] public List<string> Link { get; set; }
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+
+        [XmlElement(ElementName = "pubDate")] public string PubDate { get; set; }
+
+        [XmlElement(ElementName = "generator")]
+        public string Generator { get; set; }
+
+        [XmlElement(ElementName = "language")] public string Language { get; set; }
+        [XmlElement(ElementName = "image")] public Image Image { get; set; }
+        [XmlElement(ElementName = "item")] public List<Item> Items { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class RssModel
+    {
+        [XmlElement(ElementName = "channel")] public Channel Channel { get; set; }
+
+        [XmlAttribute(AttributeName = "content", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Content { get; set; }
+
+        [XmlAttribute(AttributeName = "wfw", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Wfw { get; set; }
+
+        [XmlAttribute(AttributeName = "dc", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Dc { get; set; }
+
+        [XmlAttribute(AttributeName = "media", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Media { get; set; }
+
+        [XmlAttribute(AttributeName = "atom", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Atom { get; set; }
+
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+    }
+}
+}
